97dea747cf056d00a35373d042342c3725422601
enclar <107608010+enclar@users.noreply.github.com>
2022-09-05 16:55:24 +0800

    set up homepage with placeholder text

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,6 +1,16 @@
+import "./Home.css";
+
 const Home = () => {
     return (
-        <div>This is Home</div>
+        <div id="home">
+            <h1 id="welcome">welcome to</h1>
+            <h1 id="ch">
+                COOPER
+                <br />
+                HEWITT
+            </h1>
+            <h1 id="online">online</h1>
+        </div>
     );
 };
 
---
+++ src/Home/Quote.jsx
@@ -0,0 +1,10 @@
+const Quote = () => {
+    return (
+        <div id="qotd">
+            <h1 id="quote">"DON'T BREAK MY LITTLE TOY!"</h1>
+            <h3 id="quote-credits">- QOTD from Micah Walter, Director of Digital and Emerging Media</h3>
+        </div>
+    );
+};
+
+export default Quote;
