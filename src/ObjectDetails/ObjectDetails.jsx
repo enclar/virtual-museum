89fa3548816d456c16548f7160828faec6ba36a9
enclar <107608010+enclar@users.noreply.github.com>
2022-09-10 11:08:09 +0800

    updated content and styling for details page. proper breadcrumbs to be added in later on

--- src/ObjectDetails/ObjectDetails.jsx
+++ src/ObjectDetails/ObjectDetails.jsx
@@ -1,13 +1,31 @@
-import { useContext } from "react";
+import React, { ReactFragment, useContext } from "react";
+import { Link } from "react-router-dom";
 import { DataContext } from "../App";
+import "./ObjectDetails.css";
 
 const ObjectDetails = () => {
     const dataContext = useContext(DataContext);
     console.log("Selected Art:", dataContext.museum.selectedArtwork);
+    const artwork = dataContext.museum.selectedArtwork;
 
     return (
         <div id="obj-details">
-            <h1>These are the object details</h1>
+            <h1 id="title">{artwork?.title}</h1>
+            <img id="img" src={artwork?.images[0]?.z?.url} alt="artwork" />
+            <div id="description">
+                <h2>{artwork?.description}</h2>
+                <h3>Dimensions: {artwork?.dimensions}</h3>
+                <h3>Medium: {artwork?.medium}</h3>
+                <br />
+                <h3>Date: {artwork?.date}</h3>
+                <h3>Year of Acquisition: {artwork?.year_acquired} </h3>
+                <h3>Medium: {artwork?.medium}</h3>
+                <br />
+                <h3 id="credit-line">{artwork?.creditline}</h3>
+            </div>
+            <Link to={"/explore/color"}>
+                <h3>BACK</h3>
+            </Link>
         </div>
     );
 };
