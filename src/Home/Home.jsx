c473f6dd6459c121b901e53b3531e55402eb8a5b
enclar <107608010+enclar@users.noreply.github.com>
2022-09-24 01:17:43 +0800

    updated home page styling

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,18 +1,24 @@
 import "./Home.css";
 
+import background_img from "../../src/images/home-background.png"
+
 const Home = () => {
     return (
         <div id="home">
             <div id="welcome">
                 <div className="circle"></div>
-                <p id="welcome">welcome to</p>
+                <div id="wt">welcome to</div>
                 <div className="circle"></div>
             </div>
             <h1 id="ch">
                 C<span>OO</span>P<span>E</span>R
                 H<span>E</span>W<span>I</span>TT
             </h1>
-            <h1 id="online">online</h1>
+            <div id="online">online</div>
+            <div id="home-img">
+                <p>Cooper Hewitt is the nation’s only museum dedicated to historic and contemporary design, with a collection of over 210,000 design objects spanning thirty centuries. Located in the landmark Andrew Carnegie mansion and boasting a beautiful public garden, Cooper Hewitt makes design come alive with unique temporary exhibitions and installations of the permanent collection.</p>
+                <img src={background_img} />
+            </div>
         </div>
     );
 };
